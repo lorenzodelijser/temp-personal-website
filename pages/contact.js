1591fa9b91adb4a8d39258976ed83377a89aa776
Lorenzo <29688706+lorenzodelijser@users.noreply.github.com>
2021-06-16 17:59:20 +0200

    Add About, Card, Contact and Projects page

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 // Components
 import Icon from '../src/components/atoms/Icon'
-// import MenuBar from '../src/components/atoms/MenuBar'
+import MenuBar from '../src/components/atoms/MenuBar'
 
 export default function Home() {
   return (
@@ -15,11 +16,11 @@ export default function Home() {
         />
       </Head>
 
-      {/* <MenuBar /> */}
+      <MenuBar />
 
       <main>
-        <h1>Lorenzo de Lijser</h1>
-        <p>A new website is in progress, for the meanwhile you can visit my social profiles.</p>
+        <h1>This is my contact page</h1>
+        <p>There is nothing to see here yet, feel free to checkout my to visit my social profiles.</p>
         <div className="social-links">
           <a href="https://twitter.com/lorenzodelijser">
             <Icon name="twitter" />
@@ -35,7 +36,13 @@ export default function Home() {
           </a>
         </div>
         <div className="sub-section">
-          <p>You can still visit my old website at: <a href="https://old.lorenzodelijser.com/">old.lorenzodelijser.com</a>.</p>
+            <Link href="/">
+                <a>
+                    <button>
+                        Return to Home
+                    </button>
+                </a>
+            </Link>
         </div>
       </main>
     </div>
